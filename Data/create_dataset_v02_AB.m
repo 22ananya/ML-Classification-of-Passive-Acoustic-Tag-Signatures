b0272f26b3239d95f5e00412d542096e47dab820
Ananya Bhardwaj <ananya22@vt.edu>
2022-11-13 16:49:53 -0500

    Updated dataset creation file, added 2nd file

--- Data/create_dataset_v02_AB.m
+++ Data/create_dataset_v02_AB.m
@@ -36,61 +36,64 @@ end
 % normalize signals to a max amplitude of 1
 X2 = X2./max(abs(X2));
 
+X = X2;
+
 % Plot to verify
 figure;
-plot(X2);
+plot(X);
 title('Check that max amplitude is 1')
 
-%% Randomly add strong reflectors to the signal 
+%% Randomly add strong reflectors and Noise to the signal 
+% reflector parameters
 fs = 10e6; % signal sample rate
 fc = 1e6; % assuming the source is a 1MHz source, so we'd expect a strong reflection at this frequency from some random object
 tau = 1e-6;
 t = 0:1/fs:(len-1)*(1/fs); % time vector equal in length to total signal
-
-% create random posiition in time for interferer/reflector
-t0 = round(len*rand(1,b))*(1/fs);
-
-% create a strong interferer 
-inf = zeros(size(X2));
-for i = 1:b
-    inf(:,i) = sin(2*pi*fc.*t).* exp(-((t - t0(i))./tau).^2);
-end
-
-% create a matrix with interferers at +6dB and another at + 12dB
-X3 = X2 + 2*inf; % +6dB interferer
-X4 = X2 + 4*inf; % +12dB interferer
-
-%consolidate in one matrix
-X = zeros(len, 3*b);
-X(:,1:b) = X2;
-X(:,b+1 : 2*b) = X3;
-X(:,2*b+1 : 3*b) = X4;
+RL = [4 2 0];
 
 
-% new Y vector - repeat three times
-Y = repmat(Y,1,3);
-
-%% Create noisy realizations of all data
 % create noise arrays at -6dB -3dB, 0dB, 3dB and 6dB -  Signal to Noise Amplitude ratio
-NL = [2, sqrt(2), 1, (1/sqrt(2)), 1/2]; % noise amplitude vectors
+NL = [2, sqrt(2), 1, (1/sqrt(2)), 1/2]; % noise amplitude vector
 
 % To create a duplicate dataset with much better SNR if needed
 NL = NL./20;
     
 X = repmat(X,1,50);
 Y = repmat(Y,1,50); 
-len = size(X,2);
+len1 = size(X,2);
 
+% Reflector
+% create random posiition in time for interferer/reflector
+t0 = round(len*rand(1,len1))*(1/fs);
+
+% create a strong interferer 
+inf = zeros(size(X));
+for i = 1:len1
+    if mod(i,3) == 0
+        inf(:,i) = RL(1)*sin(2*pi*fc.*t).* exp(-((t - t0(i))./tau).^2);
+    elseif mod(i,3) == 1
+        inf(:,i) = RL(2)*sin(2*pi*fc.*t).* exp(-((t - t0(i))./tau).^2);
+    else
+        inf(:,i) = RL(3)*sin(2*pi*fc.*t).* exp(-((t - t0(i))./tau).^2);
+    end
+end
+
+% shuffle order of interferer vector
+ind = size(inf,2);
+inf2 = inf(:,randperm(ind));
+
+% Noise
 agn = -1 + 2*rand(size(X)); % Added gaussian noise - zero mean
+len = size(X,2);
 
 for i = 1:length(NL)
     X(:,(i-1)*(len/5) + 1 : i*(len/5)) = X(:,(i-1)*(len/5) + 1 : i*(len/5)) + NL(i)*agn(:,(i-1)*(len/5) + 1 : i*(len/5));
 end
+
+X = X + inf2;
 
 %% normalize signals to a max amplitude of 1 
 X = X./max(abs(X)); 
 
 %% Save data
 
-
-%% Todo - Randomly shuffle data vector and add noise of varying amplitudes - that is also randomly shuffled
