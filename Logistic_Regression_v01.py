aa9727d625607463322834e97a61540cc55a96f3
Ananya Bhardwaj <ananya22@vt.edu>
2022-11-14 15:22:10 -0500

    updated with initial implementation

--- Logistic_Regression_v01.py
+++ Logistic_Regression_v01.py
@@ -1,23 +1,21 @@
 # Logistic Regression Approach for classifying Acoustic Signatures
 
-# Load Dependencies
+                        ## Load Dependencies
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 
 
-
-## Data Load Function
+                        ## Data Load Function
 file = 'Data\Dataset_v01.mat' # File name of Data File to be loaded
 def load_data(filename):
     data = loadmat(filename)
     return data
 
 data = load_data(file)
-X, Y = np.transpose(np.array(data['X'])), np.transpose(np.array(data['Y']))
+X, Y = np.transpose(np.array(data['X'])), np.ravel(np.array(data['Y']))
 n,d = X.shape # d is data dimension, n is total number of samples
-#X, Y = np.transpose(X,(n,d)), np.reshape(Y,(n,1))
 
 # Plot random data sample to ensure it is correctly loaded and dimensioned
 fig1, ax = plt.subplots()
@@ -26,11 +24,23 @@ ax.set_xlabel('Samples')
 ax.set_ylabel('Amplitude')
 
 
-## Build Logistic Regressor
+                        ## Build Logistic Regressor
+# Split Dataset
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
-
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=0, shuffle=True)
 print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
+# load classifier and fit data
+from sklearn.linear_model import LogisticRegression
+lgr = LogisticRegression() # shorter function name
 
+# Fit data
+lgr.fit(x_train, y_train)
 
+# Predict labels
+predictions = lgr.predict(x_test)
+
+# metrics
+from sklearn.metrics import accuracy_score
+score = accuracy_score(predictions, y_test)
+print(score)
